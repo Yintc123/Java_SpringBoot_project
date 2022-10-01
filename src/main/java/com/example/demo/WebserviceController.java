cdcdba5b1374d31d7ef266c08e6ce8686d4203dd
unknown <yiqazwsx123@gmail.com>
2022-10-01 16:15:23 +0800

    add a form for registering.

--- src/main/java/com/example/demo/WebserviceController.java
+++ src/main/java/com/example/demo/WebserviceController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,11 @@ public class WebserviceController {
 	@GetMapping("/rest")
 	public String restHello() {
 		return "Hello rest";
+	}
+	
+	@PostMapping("/member")
+	public String member() {
+		System.out.print("post method");
+		return "123";
 	}
 }
